8f07334f3be1124961f7d248227b23cfa120a33f
younggg1 <1064254619@qq.com>
2025-04-30 11:50:07 +0800

    SpringMVC:RESTful-API

---
+++ springmvc-restful-crud/src/main/java/com/atguigu/rest/crud/bean/Employee.java
@@ -0,0 +1,16 @@
+package com.atguigu.rest.crud.bean;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class Employee {
+    private Long id;
+    private String name;
+    private Integer age;
+    private String email;
+    private String gender;
+    private String address;
+    private BigDecimal salary;
+}
---
+++ springmvc-restful-crud/src/main/java/com/atguigu/rest/crud/service/EmployeeService.java
@@ -0,0 +1,41 @@
+package com.atguigu.rest.crud.service;
+
+import com.atguigu.rest.crud.bean.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    /**
+     * 根据id查询用户
+     * @param id
+     * @return
+     */
+    Employee getEmp(Long id);
+
+    /**
+     * 更新用户
+     * @param employee
+     */
+    void updateEmp(Employee employee);
+
+    /**
+     * 新增用户
+     * @param employee
+     */
+    void saveEmp(Employee employee);
+
+
+    /**
+     * 根据id删除用户
+     * @param id
+     */
+    void deleteEmp(Long id);
+
+    /**
+     * 查询所有用户
+     * @return
+     */
+    List<Employee> getList();
+
+}
