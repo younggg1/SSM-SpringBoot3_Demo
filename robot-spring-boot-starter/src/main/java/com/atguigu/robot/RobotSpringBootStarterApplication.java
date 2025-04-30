3b96ec056ff6454459a591591cdf2b1210eefff0
younggg1 <1064254619@qq.com>
2025-04-30 23:04:48 +0800

    SpringBoot

---
+++ robot-spring-boot-starter/src/main/java/com/atguigu/robot/RobotSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.robot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RobotSpringBootStarterApplication.class, args);
+    }
+
+}
